402dfd9f7981d5b4c6ef54488744a8f38d40d91f
mehdi marzban <marzban7272@gmail.com>
2025-01-03 20:52:45 +0330

    refactor: BlogCard

--- src/components/core/Author.tsx
+++ src/components/core/Author.tsx
@@ -4,7 +4,7 @@ const Author: React.FC<Readonly<{ name: string; avatarUrl: string }>> = ({ name,
     return (
         <div className="flex items-center gap-x-1">
             <Avatar src={avatarUrl} width={28} />
-            <span className="text-sm text-secondary-500">{name}</span>
+            <span className="text-sm text-secondary-500 truncate">{name}</span>
         </div>
     );
 };
--- src/components/core/BlogCard.tsx
+++ src/components/core/BlogCard.tsx
@@ -4,6 +4,7 @@ import { ClockIcon } from "@heroicons/react/24/outline";
 import { BlogType } from "@/types";
 import { Author, BlogInteractions, Button, CoverImage } from "@/components/core";
 import appTexts from "@/constants/appTexts";
+import { toPersianDigits } from "@/utils/numberFormatter";
 
 const BlogCard: React.FC<{ blog: BlogType }> = ({ blog }) => {
     return (
@@ -13,7 +14,7 @@ const BlogCard: React.FC<{ blog: BlogType }> = ({ blog }) => {
             <div>
                 {/* blog Title */}
                 <Link href={`/blogs/${blog.slug}`}>
-                    <h2 className="font-bold text-secondary-700 my-2">{blog.title}</h2>
+                    <h2 className="font-bold text-secondary-700 my-2 truncate px-1 w-full">{blog.title}</h2>
                 </Link>
 
                 {/* blog Author and ReadingTime */}
@@ -22,10 +23,10 @@ const BlogCard: React.FC<{ blog: BlogType }> = ({ blog }) => {
                     <Author avatarUrl={blog.author.avatarUrl} name={blog.author.name} />
 
                     {/* ReadingTime */}
-                    <div className="flex items-center text-[0.6rem] text-secondary-500 [&>svg]:h-5 [&>svg]:w-5">
+                    <div className="flex items-center text-[0.6rem] text-secondary-500 [&>svg]:size-5">
                         <ClockIcon />
                         <span className="ml-1">{appTexts.READING}: </span>
-                        <span className="ml-1 leading-3">{blog.readingTime}</span>
+                        <span className="ml-1 leading-3">{toPersianDigits(blog.readingTime)}</span>
                         <span>{appTexts.MINUTES}</span>
                     </div>
                 </div>
@@ -33,7 +34,7 @@ const BlogCard: React.FC<{ blog: BlogType }> = ({ blog }) => {
                 <div className="flex flex-row justify-between items-end gap-x-1 mt-2">
                     <div className="p-1 flex-1"><BlogInteractions blog={blog}/></div>
                     <Link href={`/blogs/${blog.slug}`} className="flex-1 my-0">
-                        <Button variant="secondary" className="py-1 w-full rounded-tl-none rounded-br-none rounded-bl-md">
+                        <Button variant="secondary" className="py-1 w-full rounded-tl-none rounded-br-none rounded-bl-md rounded-tr-md">
                             {appTexts.READING}
                         </Button>
                     </Link>
