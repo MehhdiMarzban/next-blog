7e8d44414637b00189b17d74e297b8a6499b3046
mehdi marzban <marzban7272@gmail.com>
2025-01-15 00:36:07 +0330

    feat: add footer file and configed in index of UI

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
 export { default as CategoryList } from "./CategoryList";
 export { default as BlogsList } from "./BlogsList";
